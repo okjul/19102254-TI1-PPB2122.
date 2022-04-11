f101f2f64dadc4b158b64cc11b92ebf830c7f204
ASUS <19102254@ittelkom-pwt.ac.id>
2022-04-11 23:30:10 +0700

    Praktikum 3

---
+++ app/src/main/java/com/Oktavia_19102254/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Oktavia_19102254.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
